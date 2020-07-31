af7a959fac7b38fb4792d268b2413f99cf18b9be
Yu <me@yuufen.com>
2020-07-31 19:22:52 +0800

    修改配置

--- src/pages/index/index.ts
+++ src/pages/index/index.ts
@@ -1,10 +1,11 @@
 // index.ts
+import { IAppOption } from '../../app';
 // 获取应用实例
-const app = getApp<IAppOption>()
+const app = getApp<IAppOption>();
 
 Page({
   data: {
-    motto: '👆作者的个人网站😊',
+    motto: 'YuuFen 的个人网站😊',
     userInfo: {},
     hasUserInfo: false,
     canIUse: wx.canIUse('button.open-type.getUserInfo'),
@@ -13,42 +14,42 @@ Page({
   bindViewTap() {
     wx.navigateTo({
       url: '../logs/logs',
-    })
+    });
   },
   onLoad() {
     if (app.globalData.userInfo) {
       this.setData({
         userInfo: app.globalData.userInfo,
         hasUserInfo: true,
-      })
+      });
     } else if (this.data.canIUse) {
       // 由于 getUserInfo 是网络请求，可能会在 Page.onLoad 之后才返回
       // 所以此处加入 callback 以防止这种情况
-      app.userInfoReadyCallback = res => {
+      app.userInfoReadyCallback = (res) => {
         this.setData({
           userInfo: res.userInfo,
           hasUserInfo: true,
-        })
-      }
+        });
+      };
     } else {
       // 在没有 open-type=getUserInfo 版本的兼容处理
       wx.getUserInfo({
-        success: res => {
-          app.globalData.userInfo = res.userInfo
+        success: (res) => {
+          app.globalData.userInfo = res.userInfo;
           this.setData({
             userInfo: res.userInfo,
             hasUserInfo: true,
-          })
+          });
         },
-      })
+      });
     }
   },
   getUserInfo(e: any) {
-    console.log(e)
-    app.globalData.userInfo = e.detail.userInfo
+    console.log(e);
+    app.globalData.userInfo = e.detail.userInfo;
     this.setData({
       userInfo: e.detail.userInfo,
       hasUserInfo: true,
-    })
+    });
   },
-})
+});
